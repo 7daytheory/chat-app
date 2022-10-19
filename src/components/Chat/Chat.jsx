465a316b5a22b284048426c774a2fa7ada0c15c1
Matthew Lowe <Matthewjlowe11@gmail.com>
2022-10-19 10:13:11 -0400

    Added Chat/Chats structure, @mui to get icons

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import LinkedCameraOutlinedIcon from '@mui/icons-material/LinkedCameraOutlined';
+import AddIcon from '@mui/icons-material/Add';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+
 
 const Chat = () => {
   return (
-    <div className="chat">Chat</div>
+    <div className="chat">
+      <div className="chatInfo">
+        <span>Stacey</span>
+        <div className="chatIcons">
+        <LinkedCameraOutlinedIcon class="icons" style={{color: "white"}}/>
+        <AddIcon class="icons" style={{color: "white"}}/>
+        <MoreHorizIcon class="icons" style={{color: "white"}}/>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/components/Chats/Chats.jsx
+++ src/components/Chats/Chats.jsx
@@ -10,6 +10,20 @@ const Chats = () => {
           <p>Message from Stacey</p>
         </div>
       </div>
+      <div className="userChat">
+        <img src="https://images.pexels.com/photos/13866617/pexels-photo-13866617.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="avatar"/>
+        <div className="userChatInfo">
+          <span>Stacey</span>
+          <p>Message from Stacey</p>
+        </div>
+      </div>
+      <div className="userChat">
+        <img src="https://images.pexels.com/photos/13866617/pexels-photo-13866617.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="avatar"/>
+        <div className="userChatInfo">
+          <span>Stacey</span>
+          <p>Message from Stacey</p>
+        </div>
+      </div>
     </div>
   )
 }
