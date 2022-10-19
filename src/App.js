d0631f37cbd33c4ed50e567875a3317d2c860b03
Matthew Lowe <Matthewjlowe11@gmail.com>
2022-10-19 14:47:31 -0400

    Create user with firebase, console error or successful creation of user

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-//import Register from "./components/Register/Register";
-import Home from "./components/Home/Home";
+import Register from "./components/Register/Register";
+//import Home from "./components/Home/Home";
 //import Login from "./components/Login/Login";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Register />
     </div>
   );
 }
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,18 +1,46 @@
 import React from 'react';
 import ContactMailOutlinedIcon from '@mui/icons-material/ContactMailOutlined';
+import {createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from '../../firebase';
 
 function Register() {
+    const handleSubmit = (e) => {   
+        e.preventDefault();
+        
+        const username = e.target[0].value;
+        const email = e.target[1].value;
+        const password = e.target[2].value;
+
+        const file = e.target[3].files[0];
+
+        console.log(username, email, password);
+
+        createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            // Signed in 
+            const user = userCredential.user;
+            
+            console.log(user);
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            
+            console.log(errorCode + " : " + errorMessage);
+        });
+    }
+
     return (
     <div className="formContainer">
         <div className="formWrapper">
             <h1 className="logo">Live-Chat App</h1>
             <span className="title">Register</span>
-            <form>
+            <form onSubmit={handleSubmit}>
             <input type="text" name="username" placeholder="username"/>
             <input type="email" name="email" placeholder="Email"/>
             <input type="password" name="password" placeholder="password"/>
             <input type="file" id="file" style={{display: "none"}}/>
-            <label for="file">
+            <label htmlFor="file">
                 <ContactMailOutlinedIcon style={{cursor: "pointer"}}/>
                 <span>Add an avatar</span>
             </label>
