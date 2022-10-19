05f0c42a8c09bc50c49ad5e5f580dc245bd0a519
Matthew Lowe <Matthewjlowe11@gmail.com>
2022-10-19 14:04:59 -0400

    Completed structure and style for chat application component

--- src/components/Message/Message.jsx
+++ src/components/Message/Message.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const Message = () => {
   return (
-    <div>Message</div>
+    <div className="message owner">
+      <div className="messageInfo">
+      <img src="https://images.pexels.com/photos/13866617/pexels-photo-13866617.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="avatar" />
+      <span>just now</span>
+      </div>
+      <div className="messageContent">
+      <p>Hello</p>
+      <img src="https://images.pexels.com/photos/13866617/pexels-photo-13866617.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="Send Error" />
+      </div>
+    </div>
   )
 }
 
