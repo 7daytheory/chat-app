b2987c693c5ee675d9ce8b09b01171d354ef85a7
Matthew Lowe <Matthewjlowe11@gmail.com>
2022-10-20 10:13:44 -0400

    Create chat entry with empty array for messages when (new) user is clicked

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,12 +1,16 @@
-import React, { useState } from 'react'
-import { collection, query, where, getDocs } from "firebase/firestore";
+import React, { useContext, useState } from 'react'
+import { collection, query, where, getDoc, setDoc, doc, updateDoc, serverTimestamp, getDocs } from "firebase/firestore";
 import { db } from '../../firebase';
+import { AuthContext } from '../context/AuthContext';
 
 const Search = () => {
 
   const [username, setUsername] = useState("");
   const [user, setUser] = useState(null);
   const [err, setErr] = useState(false);
+
+  //get current user
+  const {currentUser} = useContext(AuthContext);
   
   const handleSearch = async () => {
     const q = query(
@@ -19,13 +23,53 @@ const Search = () => {
   querySnapshot.forEach((doc) => {
    setUser(doc.data())
   });
-  } catch {
+  } catch(err) {
     setErr(true);
   }
 }
 
   const handleKey = (e) => {
     e.code === "Enter" && handleSearch(); //When user click enter it triggers handleSearch
+  }
+
+  const handleSetup = async () => {
+    //check if a chat exists, if not create new chat
+    const combineId = currentUser.uid > user.uid 
+    ? currentUser.uid + user.id 
+    : user.uid + currentUser.uid;
+
+    try {
+    const res = await getDoc(doc(db, "chats", combineId))
+
+    //exists() is a firebase method
+    if(!res.exists()) {
+    //create a chat in chats collections
+    await setDoc(doc(db, "chats", combineId), { messages: []}); //create doc with empty array for messages
+
+
+    // Update user chats
+    await updateDoc(doc(db, "userChats", currentUser.uid),{
+      [combineId+".userInfo"]: {
+        uid: user.uid,
+        displayName: user.displayName,
+        photoURL: user.photoURL
+      },
+      [combineId+".date"] : serverTimestamp()
+    });
+
+         // Update user chats
+         await updateDoc(doc(db, "userChats", user.uid),{
+          [combineId+".userInfo"]: {
+            uid: currentUser.uid,
+            displayName: currentUser.displayName,
+            photoURL: currentUser.photoURL
+          },
+          [combineId + ".date"] : serverTimestamp()
+        });
+    }
+    } catch(err) {
+      console.log(err)
+    }
   }
 
   return (
@@ -36,8 +80,8 @@ const Search = () => {
         onKeyDown={handleKey} 
         onChange={e=>setUsername(e.target.value)}/>
       </div>
-    {err && <span>User not found</span>}
-      {user && <div className="userChat">
+      {err && <span style={{color: '#fff', fontSize:'12px'}}>User not found</span>}
+      {user && <div className="userChat" onClick={handleSetup}>
         <img src={user.photoURL} alt={user.displayName}/>
         <div className="userChatInfo">
           <span>{user.displayName}</span>
