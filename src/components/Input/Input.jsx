11633e9c27542f3808ca649c4fde16642ab0f6da
Matthew Lowe <Matthewjlowe11@gmail.com>
2022-10-21 15:16:38 -0400

    Added UUID, Set messages in chats table for each user when sent

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,19 +1,78 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
 import AttachFileIcon from '@mui/icons-material/AttachFile';
 import AddPhotoAlternateIcon from '@mui/icons-material/AddPhotoAlternate';
+import { ChatContext } from '../context/ChatContext';
+import { AuthContext } from '../context/AuthContext';
+import { arrayUnion, doc, Timestamp, updateDoc } from 'firebase/firestore';
+import { v4 as uuid } from 'uuid';
+import { db, storage } from '../../firebase';
+import { getDownloadURL, ref, uploadBytesResumable } from 'firebase/storage';
 
 
 const Input = () => {
+  
+
+  const [text, setText ] = useState("");
+  const [img, setImg] = useState(null);
+
+  const {currentUser} = useContext(AuthContext);
+  const {data} = useContext(ChatContext);
+
+  const handleSend = async () => {
+      if(img) {
+
+        const storageRef = ref(storage, uuid);
+        const uploadTask = uploadBytesResumable(storageRef, img);
+
+        uploadTask.on('state_changed', 
+          (snapshot) => {
+            //setError(false);
+            //setSuccess(true);
+          }, 
+          (error) => {
+            //setError(true);
+            //setSuccess(false);
+          }, 
+          () => {
+            getDownloadURL(uploadTask.snapshot.ref).then(async (downloadURL) => {
+              await updateDoc(doc(db, "chats", data.chatId), {
+                messages: arrayUnion({
+                  id: uuid(),
+                  text,
+                  senderId: currentUser.uid,
+                  date: Timestamp.now(),
+                  img: downloadURL
+                }) 
+              })
+              
+            })
+            }); 
+
+              } else {
+            await updateDoc(doc(db, "chats", data.chatId), {
+                messages: arrayUnion({
+                  id: uuid(),
+                  text,
+                  senderId: currentUser.uid,
+                  date: Timestamp.now(),
+                }) 
+              })
+          }
+
+          setText("");
+          setImg(null);
+        }
+
   return (
     <div className="input">
-      <input type="text" placeholder="Say Something..." />
+      <input type="text" placeholder="Say Something..." onChange={e => setText(e.target.value)} value={text}/>
       <div className="send">
         <AttachFileIcon className="input-icon" />
-        <input type="file" style={{display: 'none'}} id="file" />
+        <input type="file" style={{display: 'none'}} id="file" onChange={e => setImg(e.target.files[0])}/>
         <label htmlFor="file">
           <AddPhotoAlternateIcon className="input-icon" />
         </label>
-        <button>Send</button>
+        <button onClick={handleSend}>Send</button>
       </div>
     </div>
   )
