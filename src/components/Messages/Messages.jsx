b2286acb8f7f650564d0cb95780fff62715dcd2c
Matthew Lowe <Matthewjlowe11@gmail.com>
2022-10-21 15:21:33 -0400

    Retrieve all chats that match the 2 users ID and send to Message component

--- src/components/Messages/Messages.jsx
+++ src/components/Messages/Messages.jsx
@@ -1,16 +1,29 @@
-import React from 'react'
+import { doc, onSnapshot } from 'firebase/firestore';
+import React, { useContext, useEffect, useState } from 'react'
+import { db } from '../../firebase';
+import { ChatContext } from '../context/ChatContext';
 import Message from '../Message/Message';
 
 const Messages = () => {
+
+  const [messages, setMessages] = useState([]);
+  const {data} = useContext(ChatContext);
+
+  useEffect(() => {
+    const unSub = onSnapshot(doc(db, "chats", data.chatId), (doc) => {
+      doc.exists() && setMessages(doc.data().messages)
+    })
+
+    return() => {
+      unSub()
+    }
+  })
+
   return (
     <div className="messages">
-         <Message />
-         <Message />
-         <Message />
-         <Message />
-         <Message />
-         <Message />
-         <Message />
+      {messages?.map(message => {
+        <Message key={message.id}/>
+      })}
     </div>
   )
 }
